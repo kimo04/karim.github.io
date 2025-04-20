b5400070e3c7ed09c2c2e7148b5322a01ebc9ef3
Karim Khellaf <karim.khellaf86@gmail.com>
2025-04-20 16:40:45 -0400

    change projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,32 +21,6 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={veille}
-              isBlog={false}
-              title="Veille Entreprise"
-              demoLink="https://plateforme.veille-entreprise.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={gf}
-              isBlog={false}
-              title="Gest Force"
-              demoLink="http://41.201.130.112:62345/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={nlc}
-              isBlog={false}
-              title="Ndir La Commande"
-              demoLink="https://ndirlacommande.com/"              
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
